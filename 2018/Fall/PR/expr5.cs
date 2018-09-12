cd5358a864c584aa4d6e620531cde82421974adf
Sagdeev Radel <sagdeevradel@gmail.com>
2018-09-12 22:43:00 +0300

    Main File

---
+++ 2018/Fall/PR/expr5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemExpr5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer=0;
+            int a, b;
+            a = Console.Read();
+            b = Console.Read();
+            Console.Write("Its a " + a + b);
+            if (a%4 + b%4 == 4)
+            {
+                answer = 1;
+            }
+            Console.WriteLine(answer);
+            int ThousandDifference = b / 100 - a / 100;
+            if (a%100 + b%100>100)
+            {
+                ThousandDifference += 1;
+            }
+            answer += (b - a) / 4 - ThousandDifference + ThousandDifference / 4;
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+    }
+}
