8cb6384f7dd48d678a6e0e8637c30915535c0c98
Sagdeev Radel <sagdeevradel@gmail.com>
2018-09-24 11:28:43 +0300

    Rubles and distances practics

---
+++ 2018/Fall/PR/DistanceTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DistanceTask
+{
+	public static class DistanceTask
+	{
+		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+		{
+            double distDA = Math.Sqrt(Math.Pow(x - ax,2) + Math.Pow(y - ay,2));
+            double distDB = Math.Sqrt(Math.Pow(x - bx,2) + Math.Pow(y - by,2));
+            double distAB = Math.Sqrt(Math.Pow(ax - bx,2) + Math.Pow(ay - by,2));
+
+            //скалярное произведение векторов
+            double scalarABandAD = (x - ax) * (bx - ax) + (y - ay) * (by - ay);
+            double scalarABandBD = (x - bx) * (-bx + ax) + (y - by) * (-by + ay);
+
+            if (distAB == 0) return distDA;// на случай, когда B и A имеют одинаковые координаты
+            // Если скалярное произведение >= 0, то угол острый или прямой
+            if (scalarABandAD >= 0 && scalarABandBD >= 0)
+            {
+                //находим площадь треугольника через полупериметр
+                double halfP = (distDA + distDB + distAB) / 2.0;
+                double areaOfTrian = Math.Sqrt(Math.Abs((halfP * (halfP - distDA) * (halfP - distDB) * (halfP - distAB))));
+                return (2.0 * areaOfTrian) / distAB;
+                //пытался найти площадь через стороны, не получилось, выдавало ошибку :(
+            }
+            // если скалярные произведения меньше нуля, то нам нужно взять меньшее расстояние до одной из точек
+            else if (scalarABandAD < 0 || scalarABandBD < 0)
+            //Скалярное произведение < 0  когда cosAlfa<0, то есть угол тупой
+            {
+                return Math.Min(distDA, distDB);
+            }
+            else return 0;//выполняется, если точка находится в отрезке AB
+        }
+    }
+}
---
+++ 2018/Fall/PR/PluralizeTask.cs
@@ -0,0 +1,25 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+            // Буду делать через остатки от 100 и 10
+            // При остатке 10 от деления на 100 всегда будет Рублей
+            // 11 рублей, 111 рублей, 15 рублей, 115 рублей и т.д.
+		    if (count % 100 <= 20 && count % 100 >= 10)
+            {
+                return "рублей";
+            } else //Иначе зависит от остатка от деления на 10
+            {
+                int tensRemainder = count % 10;
+                if (tensRemainder > 1 && tensRemainder < 5) // 4 рубля,3 рубля,но 5 рублей
+                    return "рубля";
+                else if (tensRemainder == 1)// рубль,просто рубль
+                    return "рубль";
+                else return "рублей";// в других случаях всегда будет рублей
+            }
+		}
+	}
+}
